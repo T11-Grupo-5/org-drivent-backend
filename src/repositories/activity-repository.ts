81e56db3df2f3db3de3955b117f9d278d1d8b089
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-10-27 16:50:28 -0300

    feat: activitys routers

---
+++ src/repositories/activity-repository.ts
@@ -0,0 +1,42 @@
+import { prisma } from '@/config';
+
+export async function FindActivityByDayId(DayId: number) {
+  return await prisma.activity.findMany({
+    where: {
+      Days: {
+        some: {
+          id: DayId,
+        },
+      },
+    },
+    include: { Users: { select: { id: true } } },
+  });
+}
+export async function getAllDays() {
+  return await prisma.day.findMany({});
+}
+
+export async function ActivityById(activityId: number) {
+  return await prisma.activity.findMany({
+    where: {
+      id: activityId,
+    },
+    include: { Users: true },
+  });
+}
+
+export async function InsertUserActivity(activityId: number, UserId: number) {
+  return prisma.activity.update({
+    where: {
+      id: activityId,
+    },
+    data: {
+      Users: {
+        connect: {
+          id: UserId,
+        },
+      },
+    },
+    include: { Users: true },
+  });
+}
